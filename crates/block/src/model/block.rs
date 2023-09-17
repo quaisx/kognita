1b844c29f1f5d96b8d0529ad4c2d37269550af20
quaisx <alex@braneframe.com>
2023-09-17 10:53:27 -0400

    [ref] refactor tx messages

--- crates/block/src/model/block.rs
+++ crates/block/src/model/block.rs
@@ -1,3 +1,11 @@
+/// Block message structure
+/// Field	Description	Size
+/// Magic no	Data field indicating that this data packet contains a BitcoinSV block. Value always 0xD9B4BEF9	4 bytes
+/// Block size	number of bytes remaining in the packet up to end of block	4 bytes
+/// Block header	consists of 6 items	80 bytes
+/// Transaction counter	positive integer VI = VarInt	1 - 9 bytes
+/// Transactions	the (non empty) list of transactions	<Transaction counter>-many transactions
+/// Source ![Block message structure](<https://wiki.bitcoinsv.io/index.php/Block#Block_message_structure>)
 #[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct Block {
@@ -20,6 +28,7 @@ pub struct Block {
 /// Time	Current block timestamp as seconds since 1970-01-01T00:00 UTC	Every few seconds	4
 /// Bits	Current target in compact format	The difficulty is adjusted (approx 2 weeks)	4
 /// Nonce	32-bit number (starts at 0)	A hash is tried (increments)	4
+/// Source ![Block header](<https://wiki.bitcoinsv.io/index.php/Block_header>)
 #[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct Header {
@@ -29,6 +38,7 @@ pub struct Header {
     pub hash_prev_block: ::prost::alloc::vec::Vec<u8>,
     #[prost(bytes = "vec", tag = "3")]
     pub hash: ::prost::alloc::vec::Vec<u8>,
+    /// Block header time stamp !\[timestamp\](<https://wiki.bitcoinsv.io/index.php/Block_timestamp>)
     #[prost(bytes = "vec", tag = "4")]
     pub timestamp: ::prost::alloc::vec::Vec<u8>,
     #[prost(bytes = "vec", tag = "5")]
